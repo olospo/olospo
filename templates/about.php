fba14babc8cc920147d5a1bf672c58ebf5b571b7
Tom Brooks <tom@olospo.co.uk>
2023-08-07 08:21:30 +0000

    Testimonial added

--- templates/about.php
+++ templates/about.php
@@ -24,29 +24,29 @@ while ( have_posts() ) : the_post(); ?>
   </div>
   <div class="container">
     <div class="twelve columns">
-    <div class="row">
-      <article class="six columns">
-        <p>Creating websites can be stressful, working with Tom is exciting and enjoyable throughout. The sites he builds are super fast, professional and pixel-perfect. Already, after two significant and successfully client-pleasing sites, Tom is my go-to Developer. I look forward to our next project.</p>
-        <strong>Chris Marsh - Think Marsh</strong>
-      </article>
-    
-    <article class="six columns">
-      <p>Developers need to be good communicators (most aren't). They need to be good planners and good timekeepers (most aren't). They need to listen and turn the project's creative vision into something that works in the real world. Tom has all of these qualities in spades and I wouldn't hesitate to recommend him for any website project.</p>
-      <strong>Andre Tomlin - Minervation</strong>
-    </article>
-    </div>
-    
-<!--     <div class="row">
-      <article class="six columns">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <strong>Meg Baldwin - The Group of Seven</strong>
-      </article>
-      <article class="six columns">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <strong>Paul Hill - A Productions</strong>
-      </article>
-    </div> -->
-    
+      <div class="row">
+        <article class="six columns">
+          <p>We worked with Tom to improve our existing WordPress website. After being told by other companies that the website needed replacing, Tom took on the challenge to help us update it, improving the performance and fixing elements that had broken or never worked well. Tom was pro-active, professional and kept us informed at all times. Tom was transparent about what was and wasn’t possible due to the age of the site and we have seen an immediate improvement in our website speed and performance. Working with Tom was very easy, and we will continue working with him on future projects.</p>
+          <strong>Extreme Design</strong>
+        </article>
+      
+        <article class="six columns">
+          <p>Developers need to be good communicators (most aren't). They need to be good planners and good timekeepers (most aren't). They need to listen and turn the project's creative vision into something that works in the real world. Tom has all of these qualities in spades and I wouldn't hesitate to recommend him for any website project.</p>
+          <strong>Andre Tomlin - Minervation</strong>
+        </article>
+      </div>
+      
+      <div class="row">
+        <article class="six columns">
+          <p>Creating websites can be stressful, working with Tom is exciting and enjoyable throughout. The sites he builds are super fast, professional and pixel-perfect. Already, after two significant and successfully client-pleasing sites, Tom is my go-to Developer. I look forward to our next project.</p>
+          <strong>Chris Marsh - Think Marsh</strong>
+        </article>
+        
+        <!-- <article class="six columns">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <strong>Meg Baldwin - The Group of Seven</strong>
+        </article> -->
+      </div>
     </div>
   </div>
 </section>
