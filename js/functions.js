080c5eadf663ac7e5332a6b23467fcac55a962dd
Tom Brooks <tom@olospo.co.uk>
2024-05-13 15:30:51 +0100

    Maintenance “go live”

--- js/functions.js
+++ js/functions.js
@@ -71,3 +71,23 @@
   });
 
 })(jQuery);
+
+// Accordian
+function accordion_ajax() {
+  var accItem = document.getElementsByClassName('accordionItem');
+  var accHD = document.getElementsByClassName('accordionItemHeading');
+  for (i = 0; i < accHD.length; i++) {
+      accHD[i].addEventListener('click', toggleItem, false);
+  }
+  function toggleItem() {
+      var itemClass = this.parentNode.className;
+      for (i = 0; i < accItem.length; i++) {
+        // accItem[i].className = 'accordionItem close';
+        this.parentNode.className = 'accordionItem close';
+      }
+      if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+      }
+  }
+}
+accordion_ajax();
---
+++ templates/maintenance.php
@@ -0,0 +1,163 @@
+<?php /* Template Name: Maintenance */
+get_header();
+
+while ( have_posts() ) : the_post(); ?>
+
+<section class="intro single">
+  <div class="container">
+    <div class="ten columns">
+      <h1>WordPress support & <span class="stand_out">maintenance</span></h1>
+    </div>
+  </div>
+</section>
+
+<section class="pricing">
+  <div class="container">
+    <div class="heading twelve columns">
+      <h2>Pricing</h2>
+      <p>Find the best solution for you.</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="price_plans">
+      <div class="plan essentials one-third column">
+        <div class="title">
+          <h3><span class="highlight">Essential</span> Support</h3>
+          <strong>£100/month</strong>
+        </div>
+        <div class="content">
+        <ul>
+          <li class="heading"><strong>Support & updates</strong></li>
+          <li><i class="fa-solid fa-check green"></i> Up to 1 hour support each month</li>
+          <li><i class="fa-solid fa-check green"></i> Monthly backups</li>
+          <li><i class="fa-solid fa-check green"></i> Monthly WordPress + Plugin updates</li>
+          <li class="heading"><strong>Website reviews</strong></li>
+          <li class="grey"><i class="fa-solid fa-check red"></i> Performance review</li>
+          <li class="grey"><i class="fa-solid fa-check red"></i> Security review</li>
+          <li class="heading"><strong>Website monitoring</strong></li>
+          <li><i class="fa-solid fa-check yellow"></i> Monthly security updates</li>
+          <li><i class="fa-solid fa-check yellow"></i> Uptime monitoring (30 min interval)</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> SSL Monitoring</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> Domain Monitoring</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> 404 Monitoring</li>
+          <li class="heading"><strong>WordPress email</strong></li>
+          <li class="grey"><i class="fa-solid fa-xmark yellow"></i> Standard PHP mailer</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> Email monitoring</li>
+          
+        </ul>
+        </div>
+      </div>
+      <div class="plan advanced one-third column">
+        <div class="title">
+          <h3><span class="highlight">Advanced</span> Support</h3>
+          <strong>£200/month</strong>
+        </div>
+        <div class="content">
+        <ul>
+          <li class="heading"><strong>Support & updates</strong></li>
+          <li><i class="fa-solid fa-check green"></i> Up to 2 hours support each month</li>
+          <li><i class="fa-solid fa-check green"></i> Weekly backups</li>
+          <li><i class="fa-solid fa-check green"></i> Weekly WordPress + Plugin updates </li>
+          <li class="heading"><strong>Website reviews</strong></li>
+          <li><i class="fa-solid fa-check yellow"></i> Monthly performance review</li>
+          <li><i class="fa-solid fa-check yellow"></i> Monthly security review</li>
+          <li class="heading"><strong>Website monitoring</strong></li>
+          <li><i class="fa-solid fa-check yellow"></i> Weekly security updates</li>
+          <li><i class="fa-solid fa-check yellow"></i> Uptime monitoring (5 min interval)</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> SSL Monitoring</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> Domain Monitoring</li>
+          <li class="grey"><i class="fa-solid fa-xmark red"></i> 404 Monitoring</li>
+          <li class="heading"><strong>WordPress email</strong></li>
+          <li><i class="fa-solid fa-check green"></i> SMTP for improved email deliverability</li>
+          <li><i class="fa-solid fa-check yellow"></i> Weekly email monitoring</li>
+          
+        </ul>
+        </div>
+      </div>
+      <div class="plan premium one-third column">
+        <div class="title">
+          <h3><span class="highlight">Premium</span> Support</h3>
+          <strong>£400/month</strong>
+        </div>
+        <div class="content">
+        <ul>
+          <li class="heading"><strong>Support & updates</strong></li>
+          <li><i class="fa-solid fa-check green"></i> Up to 4 hours support each month</li>
+          <li><i class="fa-solid fa-check green"></i> Daily backups</li>
+          <li><i class="fa-solid fa-check green"></i> Weekly WordPress + Plugin updates</li>
+          <li class="heading"><strong>Website reviews</strong></li>
+          <li><i class="fa-solid fa-check green"></i> Weekly performance review</li>
+          <li><i class="fa-solid fa-check green"></i> Weekly security review</li>
+          <li class="heading"><strong>Website monitoring</strong></li>
+          <li><i class="fa-solid fa-check green"></i> Daily security updates</li>
+          <li><i class="fa-solid fa-check green"></i> Uptime monitoring (60 second interval)</li>
+          <li><i class="fa-solid fa-check green"></i> SSL Monitoring</li>
+          <li><i class="fa-solid fa-check green"></i> Domain Monitoring</li>
+          <li><i class="fa-solid fa-xmark green"></i> 404 Monitoring</li>
+          <li class="heading"><strong>WordPress email</strong></li>
+          <li><i class="fa-solid fa-check green"></i> SMTP for improved email deliverability</li>
+          <li><i class="fa-solid fa-check green"></i> Real-time email monitoring</li>
+        </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- <section class="faq">
+  <div class="container">
+    <div class="heading twelve columns">
+      <h2>Frequently asked questions</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="accordion">
+    <article class="accordionItem close">
+      <div class="title accordionItemHeading">
+        Title
+        <span></span>
+        <span></span>
+      </div>
+      <div class="accordionItemContent">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </article>
+    <article class="accordionItem close">
+      <div class="title accordionItemHeading">
+        Title
+        <span></span>
+        <span></span>
+      </div>
+      <div class="accordionItemContent">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </article>
+    <article class="accordionItem close">
+      <div class="title accordionItemHeading">
+        Title
+        <span></span>
+        <span></span>
+      </div>
+      <div class="accordionItemContent">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </article>
+    <article class="accordionItem close">
+      <div class="title accordionItemHeading">
+        Title
+        <span></span>
+        <span></span>
+      </div>
+      <div class="accordionItemContent">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </article>
+    </div>
+  </div>
+</section> -->
+
+<?php get_template_part( 'inc/cta_contact' ); ?>
+
+<?php endwhile; // end of the loop. ?>
+<?php get_footer(); ?>
