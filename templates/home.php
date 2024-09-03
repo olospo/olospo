3b2fc566c7971444a0f71a505624c4287dcc38e2
Tom Brooks <tom@olospo.co.uk>
2024-09-03 10:20:28 +0100

    Home updates

--- templates/home.php
+++ templates/home.php
@@ -23,7 +23,7 @@ while ( have_posts() ) : the_post(); ?>
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
           <h3>WordPress development</h3>
           <p>Custom built, beautiful and reliable WordPress sites tailored precisely to your business needs.</p>
-          <p><a href="<?php echo get_site_url(); ?>/services/wordpress-development">Find out more</a></p>
+          <!-- <p><a href="<?php echo get_site_url(); ?>/services/wordpress-development">Find out more</a></p> -->
         </div>
       </div>
 
@@ -41,7 +41,7 @@ while ( have_posts() ) : the_post(); ?>
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z"/></svg>
           <h3>WordPress optimisation</h3>
           <p>Speeding up your WordPress site to boost user experience, improve load times, and boosting your SEO ranking.</p>
-          <p><a href="<?php echo get_site_url(); ?>/services/wordpress-optimisation">Find out more</a></p>
+          <!-- <p><a href="<?php echo get_site_url(); ?>/services/wordpress-optimisation">Find out more</a></p> -->
         </div>
       </div>
     </div>
@@ -60,7 +60,7 @@ while ( have_posts() ) : the_post(); ?>
           <div class="content">
             <h3>SUEZ UK</h3>
             <p>Designing and developing a scalable theme for use across the SUEZ UK websites.</p>
-            <p><a href="#">Read the case study</a></p>
+            
           </div>
         </div>
       </div>
@@ -69,8 +69,8 @@ while ( have_posts() ) : the_post(); ?>
           <a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/aspire-2.png" /></a>
           <div class="content">
             <h3>Aspire Intermediary Services</h3>
-            <p>Designing and developing a scalable theme for use across the SUEZ UK websites.</p>
-            <p><a href="#">Read the case study</a></p>
+            <p>Developed a custom built WordPress site to showcase the skills and expertise of Aspire. </p>
+           
           </div>
         </div>
       </div>
@@ -80,7 +80,6 @@ while ( have_posts() ) : the_post(); ?>
           <div class="content">
             <h3>A Productions</h3>
             <p>Designing and developing a modern, dynamic website that highlights their 35 years of TV and film work.</p>
-            <p><a href="#">Read the case study</a></p>
           </div>
         </div>
       </div>
